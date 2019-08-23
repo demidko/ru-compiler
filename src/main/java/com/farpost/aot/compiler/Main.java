7d2461e78096194a343c96ab6da40b741fb77f0e
Daniel Demidko <demidko@farpost.com>
2019-08-23 11:09:21 +1000

    .gitignore

---
+++ src/main/java/com/farpost/aot/compiler/Main.java
@@ -0,0 +1,17 @@
+import org.apache.lucene.util.CharsRef;
+import org.apache.lucene.util.fst.Builder;
+import org.apache.lucene.util.fst.CharSequenceOutputs;
+import org.apache.lucene.util.fst.FST;
+import org.apache.lucene.util.fst.Util;
+
+import java.io.IOException;
+
+/// FST<Тип Значения>
+/// Тип ключа - всегда байты или числа
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+}
